487c2ebf13aecc4f5ac985dcaa0509fb445cf5ae
ibooM <iboomblog@gmail.com>
2025-06-05 00:34:22 +0700

    Assignment

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,20 @@
 // Start coding here
+let add = (a,b) => {
+    return a+b
+};
+add(10,5);
+
+let subtract = (a,b) => {
+    return a-b
+};
+subtract(10,5)
+
+let multiply = (a,b) => {
+    return a*b
+};
+multiply(10,5)
+
+let divide = (a,b) => {
+    return a/b
+};
+divide(10,5)
